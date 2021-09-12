e5915a79a95eb1b97ed85ec000f99aa98beeb67c
Alvin Alvrahesta <alvin.alvrahesta@gmail.com>
2021-09-12 13:43:38 +0700

    Perbaikan Tampilan 2

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ function App() {
       className="flex items-center justify-center bg-blue-500"
       style={{ height: "60vh" }}
     >
-      <div className="rounded-md rounded-3xl px-48 py-20 bg-blue-50">
+      <div className="rounded-md rounded-3xl px-48 py-20 bg-white">
         <h2 className="font-extrabold tracking-tight text-gray-900 text-center">
-          <span className="block text-5xl text-blue-500">NAMA BUAH</span>
-          <span className="block text-blue-500">-------</span>
-          <p className="text-center text-2xl text-blue-500 font-normal">
+          <span className="block text-5xl text-gray-800">NAMA BUAH</span>
+          <span className="block text-gray-800">-------</span>
+          <p className="text-center text-2xl text-gray-800 font-normal">
             Kelompok 38
           </p>
         </h2>
